76d7b96098a32d1640dd1787931a489ab59c2efd
Vladislav Chernyshov <unbeliever1806@gmail.com>
2021-01-12 17:30:31 +0300

    новый инпут для регистрации

---
+++ src/components/inputs/UsernameInput.jsx
@@ -0,0 +1,22 @@
+export const UsernameInput = ({
+  inputContainerClass,
+  inputClass,
+  inputType,
+  inputPlaceholder,
+  inputValue,
+  onChange,
+}) => {
+  
+
+  return (
+    <div className={inputContainerClass}>
+      <input 
+        className={inputClass}
+        type={inputType}
+        placeholder={inputPlaceholder}
+        value={inputValue}
+        onChange={onChange}
+      />
+    </div>
+  );
+};
